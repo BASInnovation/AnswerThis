958aed7a8e002876001d0dbd52bfef7decbb5cc6
Phil Sant <philsant@Ashs-MacBook-Pro.local>
2015-10-24 19:23:28 +0100

    Added parser

--- extra/worker.js
+++ extra/worker.js
@@ -29,3 +29,72 @@ function isName(token) {
     return token !== undefined && token.match(/^[A-Za-z]+$/) !== null;
 }
 
+function parse(code) {
+
+    var tokens = tokenize(code);
+
+    var position = 0;
+
+    function peek() {
+        return tokens[position];
+    }
+
+    function consume(token) {
+        //assert.strictEqual(token, tokens[position]);
+        position++;
+    }
+
+    function parsePrimaryExpr() {
+        var t = peek();
+
+        if (isNumber(t)) {
+            consume(t);
+            return {type: "number", value: t};
+        } else if (isName(t)) {
+            consume(t);
+            return {type: "name", id: t};
+        } else if (t === "(") {
+            consume(t);
+            var expr = parseExpr();
+            if (peek() !== ")")
+                throw new SyntaxError("expected )");
+            consume(")");
+            return expr;
+        } else {
+
+            throw new SyntaxError("expected a number, a variable, or parentheses");
+        }
+    }
+
+    function parseMulExpr() {
+        var expr = parsePrimaryExpr();
+        var t = peek();
+        while (t === "*" || t === "/") {
+            consume(t);
+            var rhs = parsePrimaryExpr();
+            expr = {type: t, left: expr, right: rhs};
+            t = peek();
+        }
+        return expr;
+    }
+
+    function parseExpr() {
+        var expr = parseMulExpr();
+        var t = peek();
+        while (t === "+" || t === "-") {
+            consume(t);
+            var rhs = parseMulExpr();
+            expr = {type: t, left: expr, right: rhs};
+            t = peek();
+        }
+        return expr;
+    }
+
+    var result = parseExpr();
+
+    if (position !== tokens.length)
+        throw new SyntaxError("unexpected '" + peek() + "'");
+
+    return result;
+}
+
